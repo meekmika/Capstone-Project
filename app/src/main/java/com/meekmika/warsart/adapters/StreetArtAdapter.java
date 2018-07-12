7335481b371085cf4bc53af5ebb4d63900c65410
Mika <marikadriman@gmail.com>
2018-07-12 17:10:43 +0200

    load picture in street art list item

--- app/src/main/java/com/meekmika/warsart/adapters/StreetArtAdapter.java
+++ app/src/main/java/com/meekmika/warsart/adapters/StreetArtAdapter.java
@@ -1,12 +1,18 @@
 package com.meekmika.warsart.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.meekmika.warsart.R;
 import com.meekmika.warsart.data.model.StreetArt;
 
@@ -15,6 +21,12 @@ import java.util.List;
 public class StreetArtAdapter extends RecyclerView.Adapter<StreetArtAdapter.StreetArtViewHolder> {
 
     private List<StreetArt> streetArtData;
+    private FirebaseStorage storage;
+
+    public StreetArtAdapter() {
+        storage = FirebaseStorage.getInstance();
+    }
+
 
     @NonNull
     @Override
@@ -26,8 +38,22 @@ public class StreetArtAdapter extends RecyclerView.Adapter<StreetArtAdapter.Stre
 
     @Override
     public void onBindViewHolder(@NonNull StreetArtViewHolder holder, int position) {
-        String title = streetArtData.get(position).getTitle();
+        StreetArt streetArt = streetArtData.get(position);
+        String title = streetArt.getTitle();
+        String address = streetArt.getAddress();
+
         holder.streetArtTitle.setText(title);
+        holder.streetArtAddress.setText(address);
+
+        List<String> imageUrls = streetArtData.get(position).getImages();
+        if (imageUrls != null && imageUrls.size() != 0) {
+            String imageUrl = streetArt.getImages().get(0);
+            StorageReference storageReference = storage.getReferenceFromUrl(imageUrl);
+            Glide.with(holder.streetArtPreview.getContext())
+                    .using(new FirebaseImageLoader())
+                    .load(storageReference)
+                    .into(holder.streetArtPreview);
+        }
     }
 
     @Override
@@ -43,11 +69,13 @@ public class StreetArtAdapter extends RecyclerView.Adapter<StreetArtAdapter.Stre
 
     class StreetArtViewHolder extends RecyclerView.ViewHolder {
 
+        final ImageView streetArtPreview;
         final TextView streetArtTitle;
         final TextView streetArtAddress;
 
         StreetArtViewHolder(View view) {
             super(view);
+            streetArtPreview = view.findViewById(R.id.iv_street_art);
             streetArtTitle = view.findViewById(R.id.tv_street_art_title);
             streetArtAddress = view.findViewById(R.id.tv_street_art_address);
         }
